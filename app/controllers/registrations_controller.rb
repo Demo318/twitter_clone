e674fbd1b2b830d65ddab4bc3fa84a03c21b5b0e
Devin Mork <devin.mork@gmail.com>
2022-05-02 08:27:59 -0500

    Registration process flow.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    @user = User.new(sign_up_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path, notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user}
+      else
+        format.html { render "devise/registrations/new", status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   def sign_up_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
